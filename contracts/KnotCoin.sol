fd297b62d310d83907c8ccc1cab053dd1834a4b4
Camila Lo <camilalo.tw@gmail.com>
2022-05-20 14:37:40 +0800

    1. 修改發行量 2. 新增可燃燒機制

--- contracts/KnotCoin.sol
+++ contracts/KnotCoin.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract KnotCoin is ERC20, Ownable, ERC20Permit {
-    constructor() ERC20("Knot Coin", "KC") ERC20Permit("Knot Coin") {
-        _mint(msg.sender, 10000 * 10 ** decimals());
+contract KnotCoin is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("Knot Coin", "KC") {
+        _mint(msg.sender, 10000000 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
