2a1e22a73557408d57622269ac8e4f4be5d73d68
Camila Lo <camilalo.tw@gmail.com>
2022-05-20 14:19:40 +0800

    增加Token發行總量

--- contracts/KnotCoin.sol
+++ contracts/KnotCoin.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract KnotCoin is ERC20, Ownable, ERC20Permit {
     constructor() ERC20("Knot Coin", "KC") ERC20Permit("Knot Coin") {
-        _mint(msg.sender, 100 * 10 ** decimals());
+        _mint(msg.sender, 10000 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
